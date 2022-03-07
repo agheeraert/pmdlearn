6933ddce6e9e2c8e41c7fbba534640e635203a93
Aria Gheeraert <aria.gheeraert@gmail.com>
2022-03-07 17:15:08 +0100

    Corrected wrong inputs from vscode

--- core/model.py
+++ core/model.py
@@ -23,7 +23,8 @@ class Model():
     def scatter_dataframe(self, X_new):
         self.X_new = X_new
         self.n_components = X_new.shape[1]
-        print(enumerate(X_new.T))
+        print(list(enumerate(X_new.T)))
+        print(self.labels)
         df = pd.DataFrame({'{}{}'.format(i+1): component
                            for i, component in enumerate(X_new.T)})
         df['labels'] = self.labels
